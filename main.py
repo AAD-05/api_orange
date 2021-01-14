282c058738f9ae5b9d7797a08c1f00264d466f99
Serigne Cheikh Tidiane Sy NDIAYE <serigne.ndiaye@groupe-esigelec.org>
2021-01-14 16:16:42 +0100

    panier

--- main.py
+++ main.py
@@ -441,7 +441,6 @@ def addToCart(id,id_ut):
         Panier_produit.query.filter_by(id=panier.id,id_produit=id).update({Panier_produit.nombre: Panier_produit.nombre+1 })
         db.session.commit()
     
-    return "success "+ str(id) + "  " + str(id_ut)
 
 #Requete de récupération d'un forfait par son nom
 @app.route('/panier/<string:email>', methods=['POST'])
