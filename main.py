4bd37f34ae183148bd12ce66a559aae179ab8029
Mouhamad Al Amine NDIAYE <mouhamad.ndiaye@groupe-esigelec.org>
2021-01-14 20:31:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -336,18 +336,19 @@ def proposerTelephone():
                 if(p.note_connection >= n_connection):
                     if(p.note_batterie >= n_batterie):
                         if(p.note_puissance >= n_puissance):
-                            telephones.append({
-                                "title": p.modele,
-                                "subtitle": p.prix,
-                                "imageUrl": "https://boutiquepro.orange.fr/catalog/product/static/8/9988/9988_250x460_1_0.jpg",
-                                "buttons": [
-                                    {
-                                        "value": "https://boutiquepro.orange.fr/telephone-mobile-xiaomi-mi-10t-noir-128go.html?id="+str(p.id),
-                                        "title": "ajouter au panier",
-                                        "type": "web_url"
-                                    }
-                                ]
-                            })
+                            if(p.note_design >= n_design):
+                                telephones.append({
+                                    "title": p.modele,
+                                    "subtitle": p.prix,
+                                    "imageUrl": "https://boutiquepro.orange.fr/catalog/product/static/8/9988/9988_250x460_1_0.jpg",
+                                    "buttons": [
+                                        {
+                                            "value": "https://boutiquepro.orange.fr/telephone-mobile-xiaomi-mi-10t-noir-128go.html?id="+str(p.id),
+                                            "title": "ajouter au panier",
+                                            "type": "web_url"
+                                        }
+                                    ]
+                                })
         
     if(len(telephones)!=0):
         return jsonify(
