e94424de809fba6927de6dea36a1b5b927126b95
Mouhamad Al Amine NDIAYE <mouhamad.ndiaye@groupe-esigelec.org>
2021-01-14 23:28:37 +0100

    update propositionForfait

--- main.py
+++ main.py
@@ -391,19 +391,19 @@ def proposerForfait():
     for f in f_prix:
         if(t_giga is "4g"):
             if(f.giga_4g is not None and f.giga_4g >=  n_giga):
-                if(f.zone is not None and zone.lower() in f.zone.lower()):
-                    forfaits.append({
-                        "title": f.description,
-                        "subtitle": f.prix,
-                        "imageUrl": "https://www.francemobiles.com/actualites/image-orange-320-000-ventes-nettes-de-forfaits-mobiles-au-3eme-trimestre-2017-2017-17648-francemobiles.jpg",
-                        "buttons": [
-                            {
-                                "value": "https://boutiquepro.orange.fr/telephone-mobile-xiaomi-mi-10t-noir-128go.html",
-                                "title": "lien",
-                                "type": "web_url"
-                            }
-                        ]
-                    })
+                #if(f.zone is not None and zone.lower() in f.zone.lower()):
+                forfaits.append({
+                    "title": f.description,
+                    "subtitle": f.prix,
+                    "imageUrl": "https://www.francemobiles.com/actualites/image-orange-320-000-ventes-nettes-de-forfaits-mobiles-au-3eme-trimestre-2017-2017-17648-francemobiles.jpg",
+                    "buttons": [
+                        {
+                            "value": "https://boutiquepro.orange.fr/telephone-mobile-xiaomi-mi-10t-noir-128go.html",
+                            "title": "lien",
+                            "type": "web_url"
+                        }
+                    ]
+                })
         elif(t_giga is "5g"):
             if(f.giga_5g is not None and f.giga_5g >=  n_giga):
                 if(f.zone is not None and zone.lower() in f.zone.lower()):
