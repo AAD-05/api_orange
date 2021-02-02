baaac7546833fb640a99b9b72e0a7b2a32587a4f
Aliou Abou DIALLO <aliou.diallo@groupe-esigelec.org>
2021-02-02 19:51:44 +0100

    automatisation datawarehouse

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def maj_dashboard():
     # db.session.execute("CALL insertcalendrier();")
     #     db.session.execute(sqlalchemy.text("CALL my_proc(:param)"), param='something')
 
-    db.session.execute("CALL insertcalendrier()")
+    db.session.execute("CALL public.insertcalendrier()")
     db.session.commit()
 
 # For the scheduler, automatisation pour la BDD
