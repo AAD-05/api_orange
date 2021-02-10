fb6683ee7f8123c674fd2f163d82bcf969fcf0fa
Serigne Cheikh Tidiane Sy NDIAYE <serigne.ndiaye@groupe-esigelec.org>
2021-02-10 19:36:22 +0100

    liguey rek

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ class Utilisateur(db.Model):
     prenom=db.Column(db.String(60))
     email=db.Column(db.String(60))
     metier=db.Column(db.String(100))
+    point=db.Column(db.Integer)
 
 
     def __repr__(self):
